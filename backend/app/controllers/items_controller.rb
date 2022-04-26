7ae3dc1b4dcd6d6fc61ac8f6fdf060ebc9cafae1
Lior Brauer <liorbrauer@gmail.com>
2022-04-26 14:18:20 +0300

    the real fix

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -6,9 +6,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
 
   def index
-    @items = Item.includes(:tags)
-
-    # cool
+    @items = Item.includes(:tags, :user)
 
     @items = @items.tagged_with(params[:tag]) if params[:tag].present?
     @items = @items.sellered_by(params[:seller]) if params[:seller].present?
